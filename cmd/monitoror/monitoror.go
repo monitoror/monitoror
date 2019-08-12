302de6c7544d893ce1fa97f0f200d8ba68105ed3
Jean-Sébastien Didierlaurent <js.didierlaurent@gmail.com>
2019-08-12 13:50:18 +0200

    fix: add binary directory path for .env searching

--- cmd/monitoror/monitoror.go
+++ cmd/monitoror/monitoror.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/joho/godotenv"
 	"github.com/labstack/gommon/log"
 
@@ -16,8 +19,11 @@ func main() {
 	log.SetLevel(log.INFO)
 
 	// Load .env file
+	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+
 	_ = godotenv.Load(".env.local")
 	_ = godotenv.Load(".env")
+	_ = godotenv.Load(filepath.Join(dir, ".env"))
 
 	// Load GetConfig from File/Env
 	config := config.InitConfig()
