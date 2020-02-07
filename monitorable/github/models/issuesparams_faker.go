9c4421a0c6c63958649cd79d5679ee408bcd4773
Jean-Sébastien Didierlaurent <js.didierlaurent@gmail.com>
2020-02-07 16:58:53 +0100

    chore(core/faker/github): add faker for github

--- monitorable/github/models/issuesparams_faker.go
+++ monitorable/github/models/issuesparams_faker.go
@@ -1,10 +1,12 @@
-//+build !faker
+//+build faker
 
 package models
 
 type (
 	IssuesParams struct {
 		Query string `json:"query" query:"query"`
+
+		Values []float64 `json:"values" query:"values"`
 	}
 )
 
