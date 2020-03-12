b8d2e18c7ae96e22aa332ec3ea0d57311fb51944
Jean-Sébastien Didierlaurent <js.didierlaurent@gmail.com>
2020-03-12 17:34:12 +0100

    chore(build): fix typo in test coverage command

--- scripts/test/coverage.sh
+++ scripts/test/coverage.sh
@@ -2,6 +2,6 @@
 # Do not use this script manually, Use makefile
 
 go clean testdata ./...
-rm -f cover.out
+rm -f coverage.txt
 gotestsum -- -coverprofile=coverage.txt -covermode=atomic $(go list ./... | grep -v mocks)
 go tool cover -func coverage.txt | grep -v "100.0%"
