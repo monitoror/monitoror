daafc0eb24e25b87daf9db132f54a53391a50665
Alex-D <contact@alex-d.fr>
2020-03-15 17:05:55 +0100

    chore(script/run): add missing export before MO_ENV

--- scripts/run/run.sh
+++ scripts/run/run.sh
@@ -7,7 +7,7 @@ source ./scripts/setup-variables.sh
 # This script is used to start monitoror core #
 ###############################################
 
-# Force dev environment
-MO_ENV=${MO_ENV:-$MB_ENVIRONMENT}
+# Set environment (default: development)
+export MO_ENV=${MO_ENV:-$MB_ENVIRONMENT}
 
 go run --ldflags "$MB_GO_LDFLAGS" --tags "$MB_GO_TAGS" "${MB_SOURCE_PATH}"
