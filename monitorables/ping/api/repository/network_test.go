58976a7b041d2d11264daa2c42dc5b65d752f824
Jean-Sébastien Didierlaurent <js.didierlaurent@gmail.com>
2020-03-26 16:33:13 +0100

    test(core/ping): add integration test for ping

--- monitorables/ping/api/repository/network_test.go
+++ monitorables/ping/api/repository/network_test.go
@@ -4,10 +4,25 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	pingConfig "github.com/monitoror/monitoror/monitorables/ping/config"
+	"github.com/monitoror/monitoror/pkg/system"
 )
 
-func TestRepository_Ping(t *testing.T) {
-	// It's pain to test go-ping without interface/mock
-	// Assuming go-test already have tests ...
-	assert.True(t, true)
+// /!\ this is an integration test /!\
+// Note : It may be necessary to separate them from unit tests
+
+func TestRepository_Ping_Error(t *testing.T) {
+	pingRepository := NewPingRepository(pingConfig.Default)
+
+	ping, err := pingRepository.ExecutePing("0.0.0.0")
+
+	// I can't mock ping library, so i just test this repository
+	if system.IsRawSocketAvailable() {
+		assert.NoError(t, err)
+		assert.NotNil(t, ping)
+	} else {
+		assert.Error(t, err)
+		assert.Nil(t, ping)
+	}
 }
