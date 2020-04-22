498c55c3819a3a5f201f233804c943f6e03aab72
Jean-Sébastien Didierlaurent <js.didierlaurent@gmail.com>
2020-04-22 15:40:10 +0200

    test(core): make more mergeable register test

--- monitorables/register_test.go
+++ monitorables/register_test.go
@@ -4,15 +4,51 @@ import (
 	"testing"
 
 	"github.com/monitoror/monitoror/internal/pkg/monitorable/test"
+	coreModels "github.com/monitoror/monitoror/models"
+	azureDevOpsApi "github.com/monitoror/monitoror/monitorables/azuredevops/api"
+	githubApi "github.com/monitoror/monitoror/monitorables/github/api"
+	httpApi "github.com/monitoror/monitoror/monitorables/http/api"
+	jenkinsApi "github.com/monitoror/monitoror/monitorables/jenkins/api"
+	pingApi "github.com/monitoror/monitoror/monitorables/ping/api"
+	pingdomApi "github.com/monitoror/monitoror/monitorables/pingdom/api"
+	portApi "github.com/monitoror/monitoror/monitorables/port/api"
+	travisCIApi "github.com/monitoror/monitoror/monitorables/travisci/api"
+	"github.com/monitoror/monitoror/service/registry"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestManager_RegisterMonitorables(t *testing.T) {
 	// init Store
-	store, mockMonitorableHelper := test.InitMockAndStore()
+	store, _ := test.InitMockAndStore()
+	store.Registry = registry.NewRegistry()
+
 	manager := &Manager{store: store}
 	manager.RegisterMonitorables()
 
-	tileTypeCount := 12
-	tileGeneratorCount := 3
-	mockMonitorableHelper.TileSettingsManagerAssertNumberOfCalls(t, tileTypeCount, tileGeneratorCount, 0, 0)
+	mr := store.Registry.(*registry.MetadataRegistry)
+
+	// ------------ AZURE DEVOPS ------------
+	assert.NotNil(t, mr.TileMetadata[azureDevOpsApi.AzureDevOpsBuildTileType])
+	assert.NotNil(t, mr.TileMetadata[azureDevOpsApi.AzureDevOpsReleaseTileType])
+	// ------------ GITHUB ------------
+	assert.NotNil(t, mr.TileMetadata[githubApi.GithubCountTileType])
+	assert.NotNil(t, mr.TileMetadata[githubApi.GithubChecksTileType])
+	assert.NotNil(t, mr.GeneratorMetadata[coreModels.NewGeneratorTileType(githubApi.GithubChecksTileType)])
+	// ------------ HTTP ------------
+	assert.NotNil(t, mr.TileMetadata[httpApi.HTTPStatusTileType])
+	assert.NotNil(t, mr.TileMetadata[httpApi.HTTPRawTileType])
+	assert.NotNil(t, mr.TileMetadata[httpApi.HTTPFormattedTileType])
+	// ------------ JENKINS ------------
+	assert.NotNil(t, mr.TileMetadata[jenkinsApi.JenkinsBuildTileType])
+	assert.NotNil(t, mr.GeneratorMetadata[coreModels.NewGeneratorTileType(jenkinsApi.JenkinsBuildTileType)])
+	// ------------ PING ------------
+	assert.NotNil(t, mr.TileMetadata[pingApi.PingTileType])
+	// ------------ PINGDOM ------------
+	assert.NotNil(t, mr.TileMetadata[pingdomApi.PingdomCheckTileType])
+	assert.NotNil(t, mr.GeneratorMetadata[coreModels.NewGeneratorTileType(pingdomApi.PingdomCheckTileType)])
+	// ------------ PORT ------------
+	assert.NotNil(t, mr.TileMetadata[portApi.PortTileType])
+	// ------------ TRAVIS CI ------------
+	assert.NotNil(t, mr.TileMetadata[travisCIApi.TravisCIBuildTileType])
 }
