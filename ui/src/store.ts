b6e582c772b20f8cdcaeddc7e69b616e233fe353
Alex-D <contact@alex-d.fr>
2020-05-10 19:53:53 +0200

    fix(ui): make UI working even when served with route prefix
    fix #300

--- ui/src/store.ts
+++ ui/src/store.ts
@@ -58,7 +58,7 @@ const store: StoreOptions<RootState> = {
   },
   getters: {
     apiBaseUrl(): string {
-      const defaultApiBaseUrl = window.location.origin
+      const defaultApiBaseUrl = window.location.href
       let apiBaseUrl = getQueryParamValue('apiBaseUrl', defaultApiBaseUrl) as string
 
       apiBaseUrl = apiBaseUrl.replace(/\/+$/, '')
