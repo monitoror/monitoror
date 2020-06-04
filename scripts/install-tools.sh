012477b9bce5bdc9fa661ce615209696e1cf4d88
Alex-D <contact@alex-d.fr>
2020-06-04 17:59:36 +0200

    chore(core): move mokery to official repository

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -13,7 +13,7 @@ GO111MODULE=off go get -u github.com/GeertJohan/go.rice/rice
 
 # mockery, used by `make mocks`. Generating mock for backend
 echo "Installing mockery"
-GO111MODULE=off go get -u github.com/Willyham/mockery/.../
+GO111MODULE=off go get -u github.com/vektra/mockery/.../
 
 # revproxy, usef by `make proxy`. Start a proxy for test
 echo "Installing revproxy"
