c3617020caa2a687ab639cfdc5cd21acd221751f
Jean-Sébastien Didierlaurent <js.didierlaurent@gmail.com>
2023-02-01 15:33:42 +0100

    chore(build): fix build tools version

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -5,15 +5,15 @@ set -e
 
 # gotestsum, used by `make test`. Test utilities
 echo "Installing gotestsum"
-GO111MODULE=off go get -u gotest.tools/gotestsum
+GO111MODULE=off go get gotest.tools/gotestsum
 
 # rice, used by `make build`. Embed UI dist into go binary
 echo "Installing rice"
-GO111MODULE=off go get -u github.com/GeertJohan/go.rice/rice
+GO111MODULE=off go get github.com/GeertJohan/go.rice/rice
 
 # mockery, used by `make mocks`. Generating mock for backend
 echo "Installing mockery"
-GO111MODULE=off go get -u github.com/vektra/mockery/.../
+GO111MODULE=off go get github.com/vektra/mockery/.../
 
 # revproxy, usef by `make proxy`. Start a proxy for test
 echo "Installing revproxy"
